be5eab9b98f7453c742f4b3268883442a1dd1ecf
Carl Egbert and Caleb Stokka <caleb@>
2016-06-09 14:03:20 -0700

    create cryptosquare app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Web Development Template
+
+#### This is a lightweight repository to deploy a basic project at the beginning of each Epicodus pair and solo project. It is meant to save time and support clean builds. Current as of June 1, 2016
+
+#### By Kevin VanEvery, Cory Olson, and Patrick Lipscomb
+
+## Specs
+
+A string should be stripped of spaces, uppercase and punctuation
+* Input: "This is my friend's house."
+* Output: thisismyfriendshouse <br>
+
+A string should be returned in groups of five
+* Input: "This is my friend's house."
+* Output: thisi smyfr iends house <br>
+
+Strings whose lengths are perfect squares should be returned in cryptosquare format
+* Input: Dear Wendy
+* Output: drnew daey<br>
+
+Strings of any length should be rearranged in cryptosquare format
+* Input: "This is my friend's house."
+* Output: tsihh meoiy nusfd sirse <br>
---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+//Business Logic
+var csquare = function(text) {
+  text = text.toLowerCase().replace(/[^a-z]/g, '');
+  var colSize = Math.sqrt(text.length);
+  var createArray = function(rows) {
+    var arr = [];
+
+    for (var i=0;i<rows;i++) {
+       arr[i] = [];
+    }
+
+    return arr;
+  };
+  var cryptosquare = createArray(colSize);
+  var counter = 0;
+  for (var rowIndex = 0; rowIndex < colSize; rowIndex++) {
+    for (var colIndex = 0; colIndex < colSize; colIndex++) {
+      cryptosquare[rowIndex].push(text[counter]);
+      counter++;
+    }
+  }
+  var encryptedString = "";
+  for (var colIndex = 0; colIndex < colSize; colIndex++) {
+    for (var rowIndex = 0; rowIndex < colSize; rowIndex++) {
+      encryptedString += cryptosquare[rowIndex][colIndex];
+    }
+  }
+  var outputString = "";
+  console.log(cryptosquare);
+  for (var index = 0 ; index < encryptedString.length ; index++) {
+    outputString += encryptedString[index];
+    if ((index+1)%5 === 0) outputString += " ";
+  }
+  return outputString;
+}
+//User Interface Logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var inputText = $("#input").val();
+    var result = csquare(inputText);
+    $("#result").text(result);
+  });
+});
